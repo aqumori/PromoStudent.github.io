fd779cbf4d2d0d92bec2c952b8c5d66193de2e93
aqumori <roga_marina@mail.ru>
2024-07-25 11:45:08 +0300

    Update sw.js

--- icons/sw.js
+++ icons/sw.js
@@ -18,7 +18,7 @@ self.addEventListener ('fetch', event => {
   console. log('Fetch', event.request.url)
   event. respondWith(cacheFirst(event.request))
 })
-async function cacheFirstrequest) {
+async function cacheFirst(request) {
   const cached = await caches.match (request)
   return cached ?? await fetch (request)
 }
